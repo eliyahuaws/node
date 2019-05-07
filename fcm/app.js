96518b7bc93df4e3922f25929c16401081d97799
Lior Eliyahu <lior111888555@gmail.com>
2019-05-08 00:19:09 +0300

    Update fcm/app.js

--- fcm/app.js
+++ fcm/app.js
@@ -9,12 +9,12 @@ var app = express();
 
 app.get('/lior',function(request,response)
 {
-console.log("push send start");
+console.log("push send 1");
 
     var FCM = require('fcm-node');
     var serverKey = 'AIzaSyDxwijC7tI9nUyo7j4ViVfx9SVJyiVg9U8'; //put your server key here
     var fcm = new FCM(serverKey);
- 
+ console.log("push send 2");
     var message = { //this may vary according to the message type (single recipient, multicast, topic, et cetera)
         to: 'reEwmzGNabN8:APA91bEbh4hizDvyu9GigIPmB3mXhihKiYV6BO0e7Ba3S1Uh0U_MbEMbAFom-Vfh59HGwazbs5cbNht8MkU-P1OOQejmFV-JmvFVSA-_kkkRyuMv8KAZIdsIRjnKc1mU2jESqwVI_MtJ', 
   
@@ -29,6 +29,7 @@ console.log("push send start");
             my_another_key: 'my another value'
         }
     };
+     console.log("push send 3");
     
     fcm.send(message, function(err, response){
         if (err) {
@@ -37,6 +38,8 @@ console.log("push send start");
             console.log("Successfully sent with response: ", response);
         }
     });
+
+     console.log("push send 4");
 	 response.send("sdfsdf");
 });
 
