3f90fc0652a6e1e83cbe8562f31578dacd344587
Lior Eliyahu <lior111888555@gmail.com>
2019-05-08 00:20:41 +0300

    Update fcm/app.js

--- fcm/app.js
+++ fcm/app.js
@@ -11,36 +11,36 @@ app.get('/lior',function(request,response)
 {
 console.log("push send 1");
 
-    var FCM = require('fcm-node');
-     console.log("push send 1.2");
-    var serverKey = 'AIzaSyDxwijC7tI9nUyo7j4ViVfx9SVJyiVg9U8'; 
-      console.log("push send 1.3");//put your server key here
-    var fcm = new FCM(serverKey);
-      console.log("push send 1.4");
- console.log("push send 2");
-    var message = { //this may vary according to the message type (single recipient, multicast, topic, et cetera)
-        to: 'reEwmzGNabN8:APA91bEbh4hizDvyu9GigIPmB3mXhihKiYV6BO0e7Ba3S1Uh0U_MbEMbAFom-Vfh59HGwazbs5cbNht8MkU-P1OOQejmFV-JmvFVSA-_kkkRyuMv8KAZIdsIRjnKc1mU2jESqwVI_MtJ', 
+ //    var FCM = require('fcm-node');
+ //     console.log("push send 1.2");
+ //    var serverKey = 'AIzaSyDxwijC7tI9nUyo7j4ViVfx9SVJyiVg9U8'; 
+ //      console.log("push send 1.3");//put your server key here
+ //    var fcm = new FCM(serverKey);
+ //      console.log("push send 1.4");
+ // console.log("push send 2");
+ //    var message = { //this may vary according to the message type (single recipient, multicast, topic, et cetera)
+ //        to: 'reEwmzGNabN8:APA91bEbh4hizDvyu9GigIPmB3mXhihKiYV6BO0e7Ba3S1Uh0U_MbEMbAFom-Vfh59HGwazbs5cbNht8MkU-P1OOQejmFV-JmvFVSA-_kkkRyuMv8KAZIdsIRjnKc1mU2jESqwVI_MtJ', 
   
         
-        notification: {
-            title: 'Title of your push notification', 
-            body: 'Body of your push notification' 
-        },
+ //        notification: {
+ //            title: 'Title of your push notification', 
+ //            body: 'Body of your push notification' 
+ //        },
         
-        data: {  //you can send only notification or only data(or include both)
-            my_key: 'my value',
-            my_another_key: 'my another value'
-        }
-    };
-     console.log("push send 3");
+ //        data: {  //you can send only notification or only data(or include both)
+ //            my_key: 'my value',
+ //            my_another_key: 'my another value'
+ //        }
+ //    };
+ //     console.log("push send 3");
     
-    fcm.send(message, function(err, response){
-        if (err) {
-            console.log("Something has gone wrong!");
-        } else {
-            console.log("Successfully sent with response: ", response);
-        }
-    });
+ //    fcm.send(message, function(err, response){
+ //        if (err) {
+ //            console.log("Something has gone wrong!");
+ //        } else {
+ //            console.log("Successfully sent with response: ", response);
+ //        }
+ //    });
 
      console.log("push send 4");
 	 response.send("sdfsdf");
